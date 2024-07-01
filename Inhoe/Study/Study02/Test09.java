e593ab4ebf772adef61c449fd624207761e97460
Choi Inhoe <lomuhoe@gmail.com>
2024-07-02 00:22:35 +0900

    Inhoe Test 09 commit

---
+++ Inhoe/Study/Study02/Test09.java
@@ -0,0 +1,53 @@
+package Inhoe.Study.Study02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test09 {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 용사의 수(N)를 입력받음
+        int peaple = Integer.parseInt(br.readLine());
+        String cityList[] = br.readLine().trim().split(" ");
+        String heroPowerList[] = br.readLine().trim().split(" ");
+
+        Long sum = 0L;
+        for(int i=peaple-1 ; i >= 0 ; i--){
+            int heroPower = Integer.parseInt(heroPowerList[i]);
+            int monster = Integer.parseInt(cityList[i+1]);
+            // System.out.println("["+ i + "] heroPower : " + heroPower + ", monster : " + monster);
+
+            if(heroPower >= monster){
+                sum += monster;
+                heroPower -= monster;
+                monster = 0;
+                // System.out.println("["+ i + "] heroPower : " + heroPower + ", monster : " + monster);
+            }else{
+                monster -= heroPower;
+                sum += heroPower;
+                heroPower = 0;
+                // System.out.println("["+ i + "] heroPower : " + heroPower + ", monster : " + monster);
+            }
+
+            monster = Integer.parseInt(cityList[i]);
+            // System.out.println("["+ i + "] heroPower : " + heroPower + ", monster : " + monster);
+            if(heroPower >= monster){
+                sum += monster;
+                heroPower -= monster;
+                monster = 0;
+                // System.out.println("["+ i + "] heroPower : " + heroPower + ", monster : " + monster);
+            }else{
+                monster -= heroPower;
+                cityList[i] = Integer.toString(monster);
+                sum += heroPower;
+                heroPower = 0;
+                // System.out.println("["+ i + "] heroPower : " + heroPower + ", monster : " + monster);
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
