6699ee91c63948c8b15e2dbf6d0fdee95e7cfea9
Choi Inhoe <lomuhoe@gmail.com>
2024-07-02 12:49:29 +0900

    Inhoe Paiza C065 commit

--- Inhoe/Paiza/C/C065.java
+++ Inhoe/Paiza/C/C065.java
@@ -1,9 +1,12 @@
+package Inhoe.Paiza.C;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-public class Main {
+
+public class C065 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
